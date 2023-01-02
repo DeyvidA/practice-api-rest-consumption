2fbe35abb25d03e9756fd4cbf2c61265e077244a
DeyvidA <darauz@nicarao.agency>
2023-01-01 20:48:22 -0600

    Add related movies

--- src/main.js
+++ src/main.js
@@ -14,6 +14,9 @@ function createMovie(movies, container) {
   movies.forEach((movie) => {
     const movieContainer = document.createElement("div");
     movieContainer.classList.add("movie-container");
+    movieContainer.addEventListener("click", () => {
+      location.hash = `#movie=${movie.id}`;
+    });
 
     const movieImg = document.createElement("img");
     movieImg.classList.add("movie-img");
@@ -92,6 +95,37 @@ async function getTrendingMovies() {
   const movies = data.results;
 
   createMovie(movies, genericSection);
+}
+
+async function getMovieById(id) {
+  const { data } = await api(`/movie/${id}`);
+  const movie = data;
+
+  const movieImgUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+  headerSection.style.background = `
+    linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.35) 19.27%,
+      rgba(0, 0, 0, 0) 29.17%
+    ),
+  url(${movieImgUrl})
+  `;
+
+  movieDetailTitle.textContent = movie.title;
+  movieDetailDescription.textContent = movie.overview;
+  movieDetailScore.textContent = movie.vote_average;
+
+  createCategories(movie.genres, movieDetailCategoriesList);
+
+  getRelatedMovies(id);
+}
+
+async function getRelatedMovies(id) {
+  const { data } = await api(`/movie/${id}/recommendations`);
+  const movies = data.results;
+
+  createMovie(movies, relatedMoviesContainer);
 }
 
 getTrendingMoviesPreview();
